e346c76ccd8d696d7bcec907c4e926d8e7765c2f
EstevaoXavier <xestevao0@gmail.com>
2024-07-21 18:20:29 -0300

    Resolvendo o bug do whenReady()

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
+
+const Janela = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600
+    })
+  
+    win.loadFile('index.html')
+  }
+
+
+// Caso você use "script":"nodemon index.js" no seu
+// package.json, irá ocorrer um erro com o whenReady()
+// então use o "script":"electron ."
+app.whenReady().then(() => {
+    Janela()
+  })
